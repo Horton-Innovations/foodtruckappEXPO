4be3a20a7b4d94d89b451e5b3bb6f0d268a3e95a
Victor Horton <victorahorton@gmail.com>
2018-11-02 23:58:10 -0400

    adding authflow to app

--- screens/OtherScreen.js
+++ screens/OtherScreen.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import { ScrollView, StyleSheet, Text } from 'react-native';
 
-export default class LinksScreen extends React.Component {
+export default class OtherScreen extends React.Component {
   static navigationOptions = {
-    title: 'Links',
+    title: 'Other Screen',
   };
 
   render() {
